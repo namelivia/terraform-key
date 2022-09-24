8ed0aad978f844a233753a8bbcf748c5c8c01c4c
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-24 20:01:20 +0200

    Add var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "shared_credentials_file" {
   type = string
   description = "Path for the file containing aws credentials"
 }
+
+variable "ssh_key" {
+  type = string
+  description = "SSH key to create"
+}
