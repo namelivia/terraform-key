40048595faed895f6451fb849d26983548b12a65
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-25 18:19:18 +0200

    Remove provider declaration

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = "eu-west-3"
-  shared_credentials_file = "${var.shared_credentials_file}"
-}
-
 resource "aws_key_pair" "deployer" {
   key_name = "${var.key_name}"
   public_key = "${var.ssh_key}"
